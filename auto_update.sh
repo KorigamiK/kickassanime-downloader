eb983242df0759b7577f7e650650c2eabd9f0377
OrigamiK linux <korigamik@gmail.com>
2021-03-07 14:30:21 +0530

    add better ways to play on win and linux

--- auto_update.sh
+++ auto_update.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# change the path where you want the files to be stored
-xfce4-terminal -e 'bash -c "cd /home/origami/Documents/kickassanime-downloader/ && python3 automatic_checker.py"'
+pwd
+python3 automatic_checker.py
---
+++ play.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+pwd
+read -p "Enter args: " word
+python3 play.py $word
+echo "You entered: $word"
+read -p "" tmp
+exit 0
